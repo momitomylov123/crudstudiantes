dfc6745370e4f42b13d0ba4879b6e0a3db8eea4c
momitomylov123 <suarezsottileflorencia@esim.edu.ar>
2026-08-17 12:24:21 -0300

    data table :D

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import 'admin-lte/dist/js/adminlte.js';
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs5';
+
+window.$ = window.jQuery = $;
+
+// Conectar DataTables con jQuery
+DataTable.use($);
